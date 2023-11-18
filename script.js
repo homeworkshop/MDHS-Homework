f571505e365be4e909a38a73a8e2fa7e967f8413
homeworkshop <151309577+homeworkshop@users.noreply.github.com>
2023-11-18 01:28:20 -0700

    Optimization
    Replace var with const or let where applicable for better scoping and immutability.
    
    Instead of having isTransforming as a global variable, consider encapsulating it within a function or using a different approach to handle state.
    
    When setting the text content of an element, it's generally better to use textContent instead of innerText for performance reasons.
    Consolidate repeated code:
    
    Consolidate common functionality into functions to avoid redundancy.
    
    Store references to frequently accessed DOM elements in variables rather than looking them up multiple times.
    Use template literals for string concatenation:
    
    Use template literals for cleaner and more readable string concatenation.

--- script.js
+++ script.js
@@ -1,74 +1,65 @@
-var isTransforming = false;
+document.getElementById("submitBtn").addEventListener("click", function () {
+  submitSubject();
+});
 
 function submitSubject() {
-  var selectedSubject = document.getElementById("subject").value;
+  const selectedSubject = document.getElementById("subject").value;
   displayLoading(selectedSubject);
 }
 
 function transformText(toUppercase, text) {
-  var displayDiv = document.getElementById("displayText");
-  var symbols = "!@#$%^&*()_-+=[]{}|;:,.<>?";
+  const displayDiv = document.getElementById("displayText");
+  const symbols = "!@#$%^&*()_-+=[]{}|;:,.<>?";
 
-  // Check if transformation is already in progress
   if (isTransforming) return;
-
   isTransforming = true;
+  displayDiv.textContent = "";
 
-  // Clear the display text
-  displayDiv.innerText = "";
-
-  // Create a function to update the display text with glitch effect
   function updateText(index) {
     if (index < text.length) {
-      var currentChar = text[index];
-      var transformedChar = toUppercase ? currentChar.toUpperCase() : currentChar.toLowerCase();
-      var randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+      const currentChar = text[index];
+      const transformedChar = toUppercase ? currentChar.toUpperCase() : currentChar.toLowerCase();
+      const randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
 
-      // Apply glitch effect by randomly replacing characters with symbols
-      displayDiv.innerText += Math.random() < 0.5 ? transformedChar : randomSymbol;
+      displayDiv.textContent += Math.random() < 0.5 ? transformedChar : randomSymbol;
 
-      setTimeout(function () {
+      setTimeout(() => {
         updateText(index + 1);
-      }, 50); // Adjust the timeout for glitch speed
+      }, 50);
     } else {
-      // After the glitch effect, set the final text
-      displayDiv.innerText = text;
-      isTransforming = false; // Reset the flag
+      displayDiv.textContent = text;
+      isTransforming = false;
     }
   }
 
-  // Start the glitch effect from the first character
   updateText(0);
 }
 
 function displayLoading(selectedSubject) {
-  var displayDiv = document.getElementById("displayText");
-  var loadingText = "Loading...";
-  var loadingSpeed = 100; // Adjust the speed of the typewriter effect
+  const displayDiv = document.getElementById("displayText");
+  const loadingText = "Loading...";
+  const loadingSpeed = 100;
 
-  // Clear the display text
-  displayDiv.innerText = "";
+  displayDiv.textContent = "";
 
-  // Create a function to update the display text with typewriter effect
   function updateLoading(index) {
     if (index < loadingText.length) {
-      displayDiv.innerText += loadingText[index];
+      displayDiv.textContent += loadingText[index];
 
-      setTimeout(function () {
+      setTimeout(() => {
         updateLoading(index + 1);
       }, loadingSpeed);
     } else {
-      // After the typewriter effect, display the text specific to the subject
       displaySubjectText(selectedSubject);
     }
   }
 
-  // Start the typewriter effect from the first character
   updateLoading(0);
 }
 
 function displaySubjectText(selectedSubject) {
-  // Set content based on the selected subject
+  const displayDiv = document.getElementById("displayText");
+
   switch (selectedSubject) {
     case "spanish-1":
     case "spanish-2":
@@ -86,8 +77,3 @@ function displaySubjectText(selectedSubject) {
       transformText(true, "Unknown subject");
   }
 }
-
-// Attach event listeners using JavaScript
-document.getElementById("submitBtn").addEventListener("click", function() {
-  submitSubject();
-});
