d391ae9475778385d803d343bcee41de8b6af629
homeworkshop <151309577+homeworkshop@users.noreply.github.com>
2023-11-18 00:52:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,55 +2,86 @@ var isTransforming = false;
 
 function submitSubject() {
   var selectedSubject = document.getElementById("subject").value;
-  window.location.href = selectedSubject + ".html";
+  displayLoading(selectedSubject);
 }
 
-function transformText(toUppercase) {
-  var button = document.getElementById('submitBtn');
-  var buttonText = button.innerText;
+function transformText(toUppercase, text) {
+  var displayDiv = document.getElementById("displayText");
   var symbols = "!@#$%^&*()_-+=[]{}|;:,.<>?";
 
-  // Check if transformation is already in progress
   if (isTransforming) return;
 
   isTransforming = true;
+  displayDiv.innerText = "";
 
-  // Clear the button text
-  button.innerText = "";
-
-  // Create a function to update the button text with glitch effect
   function updateText(index) {
-    if (index < buttonText.length) {
-      var currentChar = buttonText[index];
+    if (index < text.length) {
+      var currentChar = text[index];
       var transformedChar = toUppercase ? currentChar.toUpperCase() : currentChar.toLowerCase();
       var randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
 
-      // Apply glitch effect by randomly replacing characters with symbols
-      button.innerText += Math.random() < 0.5 ? transformedChar : randomSymbol;
+      displayDiv.innerText += Math.random() < 0.5 ? transformedChar : randomSymbol;
 
       setTimeout(function () {
         updateText(index + 1);
-      }, 50); // Adjust the timeout for glitch speed
+      }, 50);
     } else {
-      // After the glitch effect, set the final text to uppercase
-      button.innerText = buttonText.toUpperCase();
-      isTransforming = false; // Reset the flag
+      displayDiv.innerText = text;
+      isTransforming = false;
     }
   }
 
-  // Start the glitch effect from the first character
   updateText(0);
+}
+
+function displayLoading(selectedSubject) {
+  var displayDiv = document.getElementById("displayText");
+  var loadingText = "Loading...";
+  var loadingSpeed = 100;
+
+  displayDiv.innerText = "";
+
+  function updateLoading(index) {
+    if (index < loadingText.length) {
+      displayDiv.innerText += loadingText[index];
+
+      setTimeout(function () {
+        updateLoading(index + 1);
+      }, loadingSpeed);
+    } else {
+      displaySubjectText(selectedSubject);
+    }
+  }
+
+  updateLoading(0);
+}
+
+function displaySubjectText(selectedSubject) {
+  switch (selectedSubject) {
+    case "spanish":
+      transformText(true, "Snap mdhs.homework with what you want done.");
+      break;
+    case "geometry":
+      transformText(true, "Snap mdhs.homework with what you want done.");
+      break;
+    case "computer-science":
+      transformText(true, "Snap mdhs.homework with what you want done.");
+      break;
+    case "english":
+      transformText(true, "Snap mdhs.homework with what you want done.");
+      break;
+    case "physical-science":
+      transformText(true, "Snap mdhs.homework with what you want done.");
+      break;
+    case "world-history":
+      transformText(true, "Snap mdhs.homework with what you want done.");
+      break;
+    default:
+      transformText(true, "Unknown subject");
+  }
 }
 
 // Attach event listeners using JavaScript
 document.getElementById("submitBtn").addEventListener("click", function() {
   submitSubject();
-});
-
-document.getElementById("submitBtn").addEventListener("mouseover", function() {
-  transformText(true);
-});
-
-document.getElementById("submitBtn").addEventListener("mouseout", function() {
-  transformText(false);
 });
