6be51283d63f39cee21fde984558acffcc0852aa
homeworkshop <151309577+homeworkshop@users.noreply.github.com>
2023-11-18 01:03:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,38 +9,47 @@ function transformText(toUppercase, text) {
   var displayDiv = document.getElementById("displayText");
   var symbols = "!@#$%^&*()_-+=[]{}|;:,.<>?";
 
+  // Check if transformation is already in progress
   if (isTransforming) return;
 
   isTransforming = true;
+
+  // Clear the display text
   displayDiv.innerText = "";
 
+  // Create a function to update the display text with glitch effect
   function updateText(index) {
     if (index < text.length) {
       var currentChar = text[index];
       var transformedChar = toUppercase ? currentChar.toUpperCase() : currentChar.toLowerCase();
       var randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
 
+      // Apply glitch effect by randomly replacing characters with symbols
       displayDiv.innerText += Math.random() < 0.5 ? transformedChar : randomSymbol;
 
       setTimeout(function () {
         updateText(index + 1);
-      }, 50);
+      }, 50); // Adjust the timeout for glitch speed
     } else {
+      // After the glitch effect, set the final text
       displayDiv.innerText = text;
-      isTransforming = false;
+      isTransforming = false; // Reset the flag
     }
   }
 
+  // Start the glitch effect from the first character
   updateText(0);
 }
 
 function displayLoading(selectedSubject) {
   var displayDiv = document.getElementById("displayText");
   var loadingText = "Loading...";
-  var loadingSpeed = 100;
+  var loadingSpeed = 100; // Adjust the speed of the typewriter effect
 
+  // Clear the display text
   displayDiv.innerText = "";
 
+  // Create a function to update the display text with typewriter effect
   function updateLoading(index) {
     if (index < loadingText.length) {
       displayDiv.innerText += loadingText[index];
@@ -49,14 +58,17 @@ function displayLoading(selectedSubject) {
         updateLoading(index + 1);
       }, loadingSpeed);
     } else {
+      // After the typewriter effect, display the text specific to the subject
       displaySubjectText(selectedSubject);
     }
   }
 
+  // Start the typewriter effect from the first character
   updateLoading(0);
 }
 
 function displaySubjectText(selectedSubject) {
+  // Set content based on the selected subject
   switch (selectedSubject) {
     case "spanish":
       transformText(true, "Snap mdhs.homework with what you want done.");
